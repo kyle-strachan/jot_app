037b605bd4289cefa350a84a58d415ae90ff6faf
reiteach <kylestrachan@outlook.com>
2025-07-24 15:18:08 -0700

    accessToken issue resolved

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 import User from "../models/users.js";
 import { signAccessToken, signRefreshToken } from "../middleware/auth.js";
 
+// Define username and password contraints
 const USERNAME_MIN_LENGTH = 3;
 const PASSWORD_MIN_LENGTH = 8;
 const REGEX_USERNAME = /^[a-zA-Z0-9_]+$/;
@@ -8,11 +9,12 @@ const REGEX_PASSWORD = /^[a-zA-Z0-9!@#$%^&*()]+$/;
 
 export async function register(req, res) {
 
+    // 1. Santize and test inputs
     let { username, password } = req.body;
-
-    username = username.trim().toLowerCase();
+    username = username.trim().toLowerCase(); // To prevent 'User' and 'user' mismatch
     password = password.trim();
 
+    // Test username against selected characters, reject if invalid
     if (!REGEX_USERNAME.test(username)) {
         res.render("index", {
             title: "Registration error",
@@ -21,6 +23,7 @@ export async function register(req, res) {
         return;
     }
 
+    // Test username against minimum length, reject if too short
     if (username.length < USERNAME_MIN_LENGTH) {
         res.render("index", {
             title: "Registration error",
@@ -29,6 +32,7 @@ export async function register(req, res) {
         return;
     }
 
+    // Test password against selected characters, reject if invalid
     if (!REGEX_PASSWORD.test(password)) {
         res.render("index", {
             title: "Registration error",
@@ -37,6 +41,7 @@ export async function register(req, res) {
         return;
     }
 
+    // Test password against minimum length, reject if too short
     if (password.length < PASSWORD_MIN_LENGTH) {
         res.render("index", {
             title: "Registration error",
@@ -45,6 +50,7 @@ export async function register(req, res) {
         return;
     }
 
+    // 2. Attempt to insert new user in db
     try {
         // Check for existing user
         const existingUser = await User.findOne({ username });
@@ -55,18 +61,19 @@ export async function register(req, res) {
             });
             return;
         }
-       
+        
+        // Hash password
         const hashedPassword = await User.hashPassword(password);
         await User.create({
             username,
             passwordHash: hashedPassword
         });
-        // console.log("User successfully created");
+        // Chose to take user directly to notes after successful registration, instead of logging in.
         login(req, res);
 
     } catch (error) {
-        // Error should be caught in check existingUser
         if (error.code === 11000) {
+            // Although should be caught in existingUser checks, it possible for an entry to be momentarily in between. Kept in code.
             res.render("index", {
             title: "Login error",
             uiMessages: { register: "Username already exists, please choose another." }
@@ -83,56 +90,80 @@ export async function register(req, res) {
 }
 
 export async function login(req, res) {
-    // BUG: username in case matters!! convert all to lower case?
-    // console.log("Login route found");
     
-    // During testing I found an abandoned session could be recovered when switching between users. This forces a logout
-    res.clearCookie("refreshToken", {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "Lax"
-    });
+    try {
+        // During testing I found an abandoned session could be recovered when switching between users.
+        // This forces a logout before proceeding with new login.
+        res.clearCookie("refreshToken", {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "Lax"
+        });
+    
+        let { username, password } = req.body;
+        
+        // Register forces lowercase username, login forced to match
+        username = username.trim().toLowerCase();
+        password = password.trim();
+        
+        const user = await User.findOne({ username });
+        // Test if user exists, reject if not. Chose to not specify which credential is incorrect in UI.
+        if (!user) {
+            res.render("index", {
+                title: "Login error",
+                uiMessages: { login: "Invalid login credentials, please try again." }
+            });
+            return;
+        }
+    
+        const isValidPassword = await user.isValidPassword(password);
+        // Test if password hash matches stored hash, reject if not.
+        if (!isValidPassword) {
+            res.render("index", {
+                title: "Login error",
+                uiMessages: { login: "Invalid login credentials, please try again." }
+            });
+            return;
+        }
+        
+        const accessToken = signAccessToken(user.id);
+        const refreshToken = signRefreshToken(user.id);
+    
+        // Issue tokens
+        res.cookie("refreshToken", refreshToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "Lax", // Mitigates CSRF
+            maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days 
+        });
+    
+        res.cookie("accessToken", accessToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "Lax", // Mitigates CSRF
+            maxAge: 15 * 60 * 1000 // 15 minutes
+        });
+    
+        // Redirect to protected route once logged in
+        res.redirect("/notes");
 
-    let { username, password } = req.body;
-    
-    // Register forces lowercase username, login forces to match
-    username = username.trim().toLowerCase();
-    password = password.trim();
-    
-    const user = await User.findOne({ username });
-    if (!user) {
+    } catch (error) {
         res.render("index", {
             title: "Login error",
-            uiMessages: { login: "Invalid login credentials, please try again." }
-        });
-        return;
+            uiMessages: { login: "An unexpected login error occurred, please try again." }
+    });  
     }
-    const isValidPassword = await user.isValidPassword(password);
-    if (!isValidPassword) {
-        res.render("index", {
-            title: "Login error",
-            uiMessages: { login: "Invalid login credentials, please try again." }
-        });
-        return;
-        // return res.status(401).json({ error: "Invalid password." });
-    }
-    const accessToken = signAccessToken(user.id);
-    const refreshToken = signRefreshToken(user.id);
-
-    res.cookie("refreshToken", refreshToken, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production", // Set secure flag in production
-        sameSite: "Lax", // Mitigates CSRF
-        maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days 
-    });
-    // res.status(200).json({ message: "Login successful", accessToken });
-    res.redirect("/notes");
-    // console.log("End of login route");
 }
 
 export async function logout(req, res) {
+    // Remove both tokens if logout is forced.
     try {
         res.clearCookie("refreshToken", {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "Lax"
+        });
+        res.clearCookie("accessToken", {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
             sameSite: "Lax"
