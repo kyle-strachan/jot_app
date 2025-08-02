5f81e5330ec41a51257df214958c156a2704bd7a
reiteach <kylestrachan@outlook.com>
2025-08-02 11:38:22 -0700

    Minor

--- middleware/rateLimit.js
+++ middleware/rateLimit.js
@@ -1,8 +1,8 @@
 import rateLimit from 'express-rate-limit';
 
 export const rateLimitMiddleware = rateLimit({
-  windowMs: 60 * 1000,
-  max: 60,
+  windowMs: 60 * 1000, // one-minute window
+  max: 30,
   standardHeaders: true,
   legacyHeaders: false,
   message: "You have exceeded the maximum requests per minute limit. Try again in a few moments.",
